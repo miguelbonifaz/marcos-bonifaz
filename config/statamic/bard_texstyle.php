aecacd50eb82250df7e8d13936e2d159333b4dac
miguelbonifaz <miguelbonifaz126@gmail.com>
2022-09-28 21:11:48 -0500

    Several improvements

--- config/statamic/bard_texstyle.php
+++ config/statamic/bard_texstyle.php
@@ -13,30 +13,30 @@ return [
 
     'styles' => [
 
-         'hero' => [
-             'type'   => 'heading',
-             'level'  => 1,
-             'name'   => 'Hero Heading',
-             'ident'  => 'H',
-             'class'  => 'hero-heading',
-             'cp_css' => 'font-family: Caslon',
-         ],
+        // 'hero' => [
+        //     'type'   => 'heading',
+        //     'level'  => 1,
+        //     'name'   => 'Hero Heading',
+        //     'ident'  => 'H',
+        //     'class'  => 'hero-heading',
+        //     'cp_css' => 'font-family: Caslon',
+        // ],
 
-         'intro' => [
-             'type'   => 'paragraph',
-             'name'   => 'Introduction',
-             'ident'  => 'I',
-             'class'  => 'introduction',
-             'cp_css' => 'font-size: 1.25em; margin-top: -0.5em',
-         ],
+        // 'intro' => [
+        //     'type'   => 'paragraph',
+        //     'name'   => 'Introduction',
+        //     'ident'  => 'I',
+        //     'class'  => 'introduction',
+        //     'cp_css' => 'font-size: 1.25em; margin-top: -0.5em',
+        // ],
 
-         'brand' => [
-             'type'   => 'span',
-             'name'   => 'Brand Text',
-             'ident'  => 'B',
-             'class'  => 'brand-text',
-             'cp_css' => 'color: dodgerblue; font-weight: 700',
-         ],
+        // 'brand' => [
+        //     'type'   => 'span',
+        //     'name'   => 'Brand Text',
+        //     'ident'  => 'B',
+        //     'class'  => 'brand-text',
+        //     'cp_css' => 'color: dodgerblue; font-weight: 700',
+        // ],
 
     ],
 
